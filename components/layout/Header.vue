c0e7f8034477bb413e9f0266f4d3f5aa743541cd
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2021-06-15 21:29:49 +0200

    Add layout header component

---
+++ components/layout/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="layout-header">
+    <div class="flex">
+      <div class="flex-1">
+        <nuxt-link to="/">
+          Johanna Groiss
+        </nuxt-link>
+      </div>
+      <div class="nav flex-1 flex justify-end">
+        <nuxt-link class="nav-item" :to="{name: 'index'}">
+          Über mich
+        </nuxt-link>
+        <nuxt-link class="nav-item" :to="{name: 'index'}">
+          Angebot
+        </nuxt-link>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+.layout-header {
+  @apply fixed w-full top-0 px-16 py-10;
+}
+
+.nav .nav-item {
+  margin-left: 2rem;
+}
+
+.nav .nuxt-link-exact-active {
+  color: $primary;
+}
+</style>
