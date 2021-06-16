5fd8b72c0fd03ef500085272a08ad43f72a1d74a
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2021-06-16 21:48:08 +0200

    Add error page

---
+++ layouts/error.vue
@@ -0,0 +1,30 @@
+<template>
+  <container>
+    <template v-if="error.statusCode === 404">
+      Seite nicht gefunden
+    </template>
+    <template v-else>
+      Oops! Ein Fehler ist aufgetreten
+    </template>
+  </container>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  layout: 'error',
+  created () {
+    console.log(this.error);
+  },
+  head () {
+    return {
+      title: `[${this.error.statusCode}] ${this.error.message}`,
+    };
+  },
+};
+</script>
