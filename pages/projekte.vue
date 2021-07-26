455d7cde4036529d080f382ebf2b16e62234a67c
Jonathan Jäckle <jjjaeckle@gmail.com>
2021-07-26 15:48:14 +0200

    Add demo for project page

--- pages/projekte.vue
+++ pages/projekte.vue
@@ -2,14 +2,29 @@
   <container>
     <heading>Projektübersicht</heading>
     <layout-section>
-      <p>
-        Projekt 1
-      </p>
+      <div v-for="project in projects" :key="project.slug">
+        {{ project.slug }}
+        <h2>{{ project.date }}</h2>
+        <nuxt-content :document="project" />
+      </div>
     </layout-section>
-        <layout-section>
+    <layout-section>
       <p>
-        Projekt 2
+        Hier Kontakt aufnehmen
       </p>
     </layout-section>
   </container>
 </template>
+
+<script>
+export default {
+  async asyncData ({ $content }) {
+    const projects = await $content('projects').where({
+      active: true,
+    }).fetch();
+    return {
+      projects,
+    };
+  },
+};
+</script>
