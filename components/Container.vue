df6490a7f0da46dfe7d273de292f763474bf7c22
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2021-07-28 18:29:47 +0200

    Improve container

--- components/Container.vue
+++ components/Container.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto px-4 sm:px-20 md:px-20 lg:px-24 lg:px-28 xl:px-36 2xl:px-40">
+  <div class="container">
     <slot />
   </div>
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        md: '3rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+  },
+};
